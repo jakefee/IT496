ab66ced570b6ab2cc7a25570a6aaa9b66258b45d
Jake Fee <jake.fee@mnsu.edu>
2015-03-30 12:03:35 -0500

    March 30
    Assignment 1 (triangle)

---
+++ assignment1.php
@@ -0,0 +1,38 @@
+<?php
+	#find perimeter, area, sine, cosine, tangent of a triangle
+	$side1 = 14.8;
+	$side2 = 20.3;
+	$side3 = 25.3;
+	$angle1 = 36;
+	$angle2 = 53;
+	$angle3 = 91;
+
+	function perimeter($a, $b, $c) {
+		$perimeter = $a + $b + $c;
+		return $perimeter;
+	}
+
+	function area($a, $b, $c) {
+		#Heron's Formula
+		$p = $a + $b + $c;
+		$s = $p / 2;
+		$area = sqrt(($s) * ($s - $a) * ($s - $b) * ($s - $c));
+		return $area;
+	}
+
+	#Perimeter and Area
+	echo "Perimeter: " . perimeter($side1, $side2, $side3);
+	echo "<br>Area: " . area($side1, $side2, $side3);
+	#Sine
+	echo "<br><br>Sine of Angle 1: " . sin($angle1);
+	echo "<br>Sine of Angle 2: " . sin($angle2);
+	echo "<br>Sine of Angle 3: " . sin($angle3);
+	#Cosine
+	echo "<br><br>Cosine of Angle 1: " . cos($angle1);
+	echo "<br>Cosine of Angle 2: " . cos($angle2);
+	echo "<br>Cosine of Angle 3: " . cos($angle3);
+	#Tangent
+	echo "<br><br>Tangent of Angle 1: " . tan($angle1);
+	echo "<br>Tangent of Angle 2: " . tan($angle2);
+	echo "<br>Tangent of Angle 3: " . tan($angle3);
+?>
